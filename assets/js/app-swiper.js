58f26982159388af92273294d55555cd90b21260
Cirhan Yaqoob <khanirhan@gmail.com>
2021-12-15 20:14:17 +0530

    Testimonials section

--- assets/js/app-swiper.js
+++ assets/js/app-swiper.js
@@ -1,6 +1,21 @@
 import Swiper from 'https://unpkg.com/swiper@7/swiper-bundle.esm.browser.min.js';
 
-const swiper = new Swiper('#exploreSwiper', {
+/*
+ * Testimonials section swiper
+ */
+const swiper1 = new Swiper('#testimonialsSwiper', {
+    spaceBetween: 0,
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.swiper-btn-next',
+        prevEl: '.swiper-btn-prev',
+    },
+});
+
+/*
+ * Explore section swiper
+ */
+const swiper2 = new Swiper('#exploreSwiper', {
     spaceBetween: 16,
     slidesPerView: 1,
     breakpoints: {
