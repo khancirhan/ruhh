e75e1559255e7a0e33e1ce9b383ee4938ea76f84
Cirhan Yaqoob <khanirhan@gmail.com>
2021-12-28 18:43:03 +0530

    Refractor navbar booking and footer blocks

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,5 @@
+$('#navbarPlaceholder').load('_navbar.html');
+$('#bookingPlaceholder').load('_booking.html');
+$('#footerPlaceholder').load('_footer.html');
+
 $('#year').text(new Date().getFullYear());
